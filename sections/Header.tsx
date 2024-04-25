ee7cd5e97401cbb389ff51eca5a8804f347a701a
Leandro Albino Madriaga <tumbumcum1@gmail.com>
2024-04-25 01:20:48 +0000

    Release Staging environment with 3 changes made by Leandro Albino Madriaga

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -1,138 +1,23 @@
 
-interface FancyGreenComponentProps {
+interface Props {
   /**
-  * @description The source of the logo.
+  * @description The description of name.
   */
-  logo?: {
-    src: string;
-    alt: string;
-  };
+  name?: string;
   /**
-  * @description The navigation links and buttons.
+  * @description The description of image.
   */
-  navigation?: {
-    links: {
-      label: string;
-      url: string;
-    }[];
-    buttons: CTA[];
-  };
+  image?: string;
 }
 
-export default function FancyGreenComponent({
-  logo = {
-    src:
-      "https://your-image-source.jpg",
-    alt: "Logo",
-  },
-  navigation = {
-    links: [
-      { label: "Home", url: "/" },
-      { label: "About us", url: "/" },
-      { label: "Pricing", url: "/" },
-      { label: "Contact", url: "/" },
-    ],
-    buttons: [
-      { id: "change-me-1", href: "/", text: "Change me", outline: false },
-      { id: "change-me-2", href: "/", text: "Change me", outline: true },
-    ],
-  },
-}: FancyGreenComponentProps) {
+export default function Section({ name = "Capy", image = "capy.jpg" }: Props) {
   return (
-    <nav class="drawer drawer-end bg-green-500">
-      <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
-
-      {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
-        <a href="/">
-          <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
-        </a>
-
-        <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
-            {navigation.links.map((link) => (
-              <li>
-                <a
-                  href={link.url}
-                  aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
-                >
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <ul class="flex gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item.id}
-                id={item.id}
-                href={item.href ?? "#"}
-                target={item.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-
-        <label
-          htmlFor="mobile-drawer-nav"
-          class="flex lg:hidden btn btn-ghost drawer-button"
-        >
-          <Icon id="Bars3" size={24} strokeWidth={0.1} />
-        </label>
+    <div class="bg-white p-8 rounded-md shadow-md">
+      <img src={image} alt="Component Image" class="mb-4">
+      <h2 class="text-2xl font-semibold mb-4">{name}</h2>
+      <div class="p-4 bg-gray-200">
+        <p class="text-gray-600">Add your text or other components here</p>
       </div>
-
-      {/* sidebar */}
-      <aside class="drawer-side z-50">
-        {/* Close when clicking on overlay */}
-        <label
-          htmlFor="mobile-drawer-nav"
-          aria-label="close sidebar"
-          class="drawer-overlay"
-        />
-
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
-          <a class="p-4" href="/">
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
-          </a>
-
-          <ul class="menu">
-            {navigation?.links.map((link) => (
-              <li>
-                <a href={link.url} aria-label={link.label}>
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-
-          <ul class="p-4 flex items-center gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item.id}
-                id={item.id}
-                href={item.href ?? "#"}
-                target={item.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-      </aside>
-    </nav>
-  );
+    </div>
+  )
 }
